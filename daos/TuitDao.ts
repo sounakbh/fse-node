6ab08faab9bd31c12863dacfdf7d98eaae0d371e
sounakbh <sounakbh@gmail.com>
2022-02-16 19:46:01 -0500

    resolve additional conflicts

--- daos/TuitDao.ts
+++ daos/TuitDao.ts
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import TuitModel from "../mongoose/TuitModel";
-import Tuit from "../models/Tuit";
-import TuitDaoI from "../interfaces/TuitDaoI";
-
-/**
- * Implements Data Access Object managing data storage
- * of Tuits
- * @implements {TuitDaoI} TuitDaoI
- * @property {TuitDao} tuitDao Private single instance of TuitDao
- */
-export default class TuitDao implements TuitDaoI {
-  private static tuitDao: TuitDao | null = null;
-  public static getInstance = (): TuitDao => {
-    if (TuitDao.tuitDao === null) {
-      TuitDao.tuitDao = new TuitDao();
-    }
-    return TuitDao.tuitDao;
-  };
-  private constructor() {}
-
-  createTuit = async (tuit: Tuit): Promise<Tuit> => TuitModel.create(tuit);
-  findAllTuits = async (): Promise<Tuit[]> => TuitModel.find();
-  findTuitsByUser = async (uid: string): Promise<Tuit[]> =>
-    TuitModel.find({ postedBy: uid });
-  findTuitById = async (tid: string): Promise<any> => TuitModel.findById(tid);
-  updateTuit = async (uid: string, tuit: Tuit): Promise<any> =>
-    TuitModel.updateOne({ _id: uid }, { $set: tuit });
-  deleteTuit = async (uid: string): Promise<any> =>
-    TuitModel.deleteOne({ _id: uid });
-}
-=======
 /**
  * @file Implements DAO managing data storage of tuits. Uses mongoose TuitModel
  * to integrate with MongoDB
@@ -43,30 +11,24 @@ import TuitDaoI from "../interfaces/TuitDaoI";
  * of Users
  * @property {UserDao} userDao Private single instance of UserDao
  */
-export default class TuitDao implements TuitDaoI{
-    private static tuitDao: TuitDao | null = null;
-    public static getInstance = (): TuitDao => {
-        if(TuitDao.tuitDao === null) {
-            TuitDao.tuitDao = new TuitDao();
-        }
-        return TuitDao.tuitDao;
+export default class TuitDao implements TuitDaoI {
+  private static tuitDao: TuitDao | null = null;
+  public static getInstance = (): TuitDao => {
+    if (TuitDao.tuitDao === null) {
+      TuitDao.tuitDao = new TuitDao();
     }
-    private constructor() {}
-    findAllTuits = async (): Promise<Tuit[]> =>
-        TuitModel.find();
-    findAllTuitsByUser = async (uid: string): Promise<Tuit[]> =>
-        TuitModel.find({postedBy: uid});
-    findTuitById = async (uid: string): Promise<any> =>
-        TuitModel.findById(uid)
-            .populate("postedBy")
-            .exec();
-    createTuitByUser = async (uid: string, tuit: Tuit): Promise<Tuit> =>
-        TuitModel.create({...tuit, postedBy: uid});
-    updateTuit = async (uid: string, tuit: Tuit): Promise<any> =>
-        TuitModel.updateOne(
-            {_id: uid},
-            {$set: tuit});
-    deleteTuit = async (uid: string): Promise<any> =>
-        TuitModel.deleteOne({_id: uid});
+    return TuitDao.tuitDao;
+  };
+  private constructor() {}
+  findAllTuits = async (): Promise<Tuit[]> => TuitModel.find();
+  findAllTuitsByUser = async (uid: string): Promise<Tuit[]> =>
+    TuitModel.find({ postedBy: uid });
+  findTuitById = async (uid: string): Promise<any> =>
+    TuitModel.findById(uid).populate("postedBy").exec();
+  createTuitByUser = async (uid: string, tuit: Tuit): Promise<Tuit> =>
+    TuitModel.create({ ...tuit, postedBy: uid });
+  updateTuit = async (uid: string, tuit: Tuit): Promise<any> =>
+    TuitModel.updateOne({ _id: uid }, { $set: tuit });
+  deleteTuit = async (uid: string): Promise<any> =>
+    TuitModel.deleteOne({ _id: uid });
 }
->>>>>>> software-engineering-node/a2
--- server.ts
+++ server.ts
@@ -6,11 +6,11 @@
  *     <li>tuits</li>
  *     <li>likes</li>
  * </ul>
- * 
+ *
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
  * service
  */
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
 import CourseController from "./controllers/CourseController";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
@@ -26,22 +26,17 @@ const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
-mongoose.connect(connectionString);
+// mongoose.connect(connectionString);
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome!'));
+app.get("/", (req: Request, res: Response) => res.send("Welcome!"));
 
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
+app.get("/add/:a/:b", (req: Request, res: Response) =>
+  res.send(req.params.a + req.params.b)
+);
 
-<<<<<<< HEAD
-const userController = UserController.getInstance(app);
-const tuitController = TuitController.getInstance(app);
-
-=======
 // create RESTful Web service API
 const courseController = new CourseController(app);
 const userController = UserController.getInstance(app);
@@ -52,6 +47,5 @@ const likesController = LikeController.getInstance(app);
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku if available.
  */
->>>>>>> software-engineering-node/a2
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
